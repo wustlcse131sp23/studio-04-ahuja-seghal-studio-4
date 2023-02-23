99310fa59bed6151ae6400fa3b9c4461136f2c80
saiveeahuja <saiveeahuja@localhost>
2023-02-23 12:19:55 -0600

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setPenColor(200, 0, 200);
+		StdDraw.filledRectangle(3, 3, 3, 3);
 		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
+		StdDraw.filledCircle(0.5, 0.5, 0.02);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,37 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		
+		int r = in.nextInt(); 
+		int g = in.nextInt();
+		int b = in.nextInt();
+		Color black = new Color (r, g, b); 
+		StdDraw.setPenColor(black);
+		
+		boolean fill = in.nextBoolean(); 
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble(); 
+		double w = in.nextDouble(); 
+		double h = in.nextDouble(); 
+		
+		if  (fill == false) {
+		StdDraw.rectangle(x, y, w, h);
+		} 
+		else {
+			StdDraw.filledRectangle(x, y, w, h);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
